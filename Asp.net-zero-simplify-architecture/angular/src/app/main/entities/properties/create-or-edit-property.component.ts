42e0d141c3f12b4130795fb8ad55661b0ec95813
Mehran Ayub <mehran.ayub@stgsystems.com>
2023-11-30 00:27:56 +0500

    design minor changes

--- Asp.net-zero-simplify-architecture/angular/src/app/main/entities/properties/create-or-edit-property.component.ts
+++ Asp.net-zero-simplify-architecture/angular/src/app/main/entities/properties/create-or-edit-property.component.ts
@@ -1,37 +1,44 @@
-﻿import { Component, ViewChild, Injector, Output, EventEmitter, OnInit, ElementRef} from '@angular/core';
-import { ModalDirective } from 'ngx-bootstrap';
-import { finalize } from 'rxjs/operators';
-import { PropertiesServiceProxy, CreateOrEditPropertyDto, PropertyStatusDto, PropertyPropertyTypeLookupTableDto } from '@shared/service-proxies/service-proxies';
-import { AppComponentBase } from '@shared/common/app-component-base';
-import * as moment from 'moment';
-import { ActivatedRoute, Router } from '@angular/router';
-import { appModuleAnimation } from '@shared/animations/routerTransition';
-import {Observable} from "@node_modules/rxjs";
-
-
-
-
-
+﻿import {
+    Component,
+    ViewChild,
+    Injector,
+    Output,
+    EventEmitter,
+    OnInit,
+    ElementRef,
+} from "@angular/core";
+import { ModalDirective } from "ngx-bootstrap";
+import { finalize } from "rxjs/operators";
+import {
+    PropertiesServiceProxy,
+    CreateOrEditPropertyDto,
+    PropertyStatusDto,
+    PropertyPropertyTypeLookupTableDto,
+} from "@shared/service-proxies/service-proxies";
+import { AppComponentBase } from "@shared/common/app-component-base";
+import * as moment from "moment";
+import { ActivatedRoute, Router } from "@angular/router";
+import { appModuleAnimation } from "@shared/animations/routerTransition";
+import { Observable } from "@node_modules/rxjs";
 
 @Component({
-    templateUrl: './create-or-edit-property.component.html',
-    animations: [appModuleAnimation()]
+    templateUrl: "./create-or-edit-property.component.html",
+    animations: [appModuleAnimation()],
 })
-export class CreateOrEditPropertyComponent extends AppComponentBase implements OnInit {
+export class CreateOrEditPropertyComponent
+    extends AppComponentBase
+    implements OnInit
+{
     active = false;
     saving = false;
-    propertyId:string;
-    isEditMode:boolean=false;
+    propertyId: string;
+    isEditMode: boolean = false;
     property: CreateOrEditPropertyDto = new CreateOrEditPropertyDto();
-    PropertyStatus=PropertyStatusDto;
-
-
-
-
+    PropertyStatus = PropertyStatusDto;
 
     constructor(
         injector: Injector,
-        private _activatedRoute: ActivatedRoute,        
+        private _activatedRoute: ActivatedRoute,
         private _propertiesServiceProxy: PropertiesServiceProxy,
         private _router: Router
     ) {
@@ -39,83 +46,71 @@ export class CreateOrEditPropertyComponent extends AppComponentBase implements O
     }
 
     ngOnInit(): void {
-        this.show(this._activatedRoute.snapshot.queryParams['id']);
+        this.show(this._activatedRoute.snapshot.queryParams["id"]);
         this.getPropertyTypes();
-        
     }
 
     show(propertyId?: string): void {
-        this.propertyId=propertyId;
+        this.propertyId = propertyId;
         if (!propertyId) {
             this.property = new CreateOrEditPropertyDto();
             this.property.id = propertyId;
-            this.isEditMode=false;
+            this.isEditMode = false;
 
             this.active = true;
         } else {
-            this._propertiesServiceProxy.getPropertyForEdit(propertyId).subscribe(result => {
-                this.property = result.property;
-                this.isEditMode=true;
+            this._propertiesServiceProxy
+                .getPropertyForEdit(propertyId)
+                .subscribe((result) => {
+                    this.property = result.property;
+                    this.isEditMode = true;
 
-
-                this.active = true;
-            });
+                    this.active = true;
+                });
         }
-        
-         
     }
-    
+
     save(): void {
-        this.saving = true; 
-        this._propertiesServiceProxy.createOrEdit(this.property)
-            .pipe(finalize(() => {
+        this.saving = true;
+        this._propertiesServiceProxy
+            .createOrEdit(this.property)
+            .pipe(
+                finalize(() => {
+                    this.saving = false;
+                })
+            )
+            .subscribe((x) => {
+                this.propertyId = x;
                 this.saving = false;
-            }))
-            .subscribe(x => {
-                this.propertyId=x;
-                 this.saving = false;               
-                 this.notify.info(this.l('SavedSuccessfully'));
+                this.notify.info(this.l("SavedSuccessfully"));
                 //  this.message.success("Property record saved successfully");
-                 setTimeout(() => {
-                 this._router.navigate( ['/app/main/entities/properties']);
-                 }, 1500);
-            })
+                setTimeout(() => {
+                    this._router.navigate(["/app/main/entities/properties"]);
+                }, 1500);
+            });
     }
-    propertyTypeLookupList:PropertyPropertyTypeLookupTableDto[]=[];
-    getPropertyTypes():void{
-        this._propertiesServiceProxy.getAllPropertyTypeForTableDropdown().subscribe((result)=>{
-            this.propertyTypeLookupList=result;
-        });
+    propertyTypeLookupList: PropertyPropertyTypeLookupTableDto[] = [];
+    getPropertyTypes(): void {
+        this._propertiesServiceProxy
+            .getAllPropertyTypeForTableDropdown()
+            .subscribe((result) => {
+                this.propertyTypeLookupList = result;
+                this.property.propertyTypeId =
+                    this.propertyTypeLookupList[0].id;
+            });
     }
     // saveAndNew(): void {
     //     this.saving = true;
-        
+
     //     this._propertiesServiceProxy.createOrEdit(this.property)
     //         .pipe(finalize(() => {
     //             this.saving = false;
     //         }))
     //         .subscribe(x => {
     //             this.propertyId=x;
-    //             this.saving = false;               
+    //             this.saving = false;
     //             this.notify.info(this.l('SavedSuccessfully'));
     //             this.property = new CreateOrEditPropertyDto();
     //         });
     // }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
